c0d4484e02b0d60d2d4265b4bb378bd1833f014c
Namir <namirh14x@gmail.com>
2023-07-23 03:46:08 -0400

    cmon work

--- responses.py
+++ responses.py
@@ -29,15 +29,15 @@ def handle_responses(msg) -> discord.Embed:
             if (p_msg.split()[1] in line) and (p_msg.split()[2] in line):
                 player_info = line.split(", ")
                 player_name, player_club, player_nationality, player_value, player_imageURL, player_id = player_info
-                break
-        embed = discord.Embed(
-            title=player_name,
-            description=player_club + "\n" + player_nationality,
-            color=0xAF0000
-        )
-        
-        embed.add_field(name= player_value, value="", inline=False)
-        embed.set_image(url=player_imageURL)
-        embed.set_footer(text="Football Roll Bot, " + player_id)
-        
-        return embed
+                
+                embed = discord.Embed(
+                    title=player_name,
+                    description=player_club + "\n" + player_nationality,
+                    color=0xAF0000
+                )
+                
+                embed.add_field(name=player_value, value="", inline=False)
+                embed.set_image(url=player_imageURL)
+                embed.set_footer(text="Football Roll Bot, " + player_id)
+
+                return embed

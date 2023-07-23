25d9e8d0c9200090bf949391a5e671f6a8134380
Namir <namirh14x@gmail.com>
2023-07-23 00:08:00 -0400

    updated responses.py

--- responses.py
+++ responses.py
@@ -15,11 +15,11 @@ def handle_responses(msg) -> discord.Embed:
         embed = discord.Embed(
             title=player_name,
             description=player_club + "\n" + player_nationality,
-            color=0xAF0000,
-            footer="Football Roll Bot, " + player_id
+            color=0xAF0000
         )
         
         embed.add_field(name= player_value, value="", inline=False)
         embed.set_image(url=player_imageURL)
+        embed.set_footer("Football Roll Bot, " + player_id)
 
         return embed

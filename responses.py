f7b52197f5f802c1a06ac8f9bb260998262321ff
Namir <namirh14x@gmail.com>
2023-07-28 01:04:13 -0400

    Adadaa

--- responses.py
+++ responses.py
@@ -162,7 +162,7 @@ def handle_responses(msg, user) -> discord.Embed:
         print(sel_player)
         if correct_player and correct_pos:
             for field1 in embed.fields:
-                if field1.name.lower() == p_msg.split()[1]:
+                if field1.name.strip().lower() == p_msg.split()[1]:
                     field1.value = sel_player
         
         return embed

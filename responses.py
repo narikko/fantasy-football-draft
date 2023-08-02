54eb0199c6ad26dbc6ee15d90cd421034cec2490
Namir <namirh14x@gmail.com>
2023-08-02 00:21:10 -0400

    asgfG

--- responses.py
+++ responses.py
@@ -49,7 +49,7 @@ async def handle_responses(msg, user_msg, user) -> discord.Embed:
             if user_upgrades[user.id][0] != 0:
                 print(num_player_club)
                 print(stadium_upgrades[user_upgrades[user.id][0]])
-                club_upgrade_chance = round(((num_player_club / 18141) * 10000) + (stadium_upgrades[user_upgrades[user.id][0]] * 100))
+                club_upgrade_chance = round(((num_player_club / 18141) * 10000) + (stadium_upgrades[user_upgrades[user.id][0] - 1] * 100))
                 club_chance = random.randint(0, 10000)
                 
                 print(club_upgrade_chance)

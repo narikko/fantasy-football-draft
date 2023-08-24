e5ba8bc603f00283621331112af058f8878c342e
Namir <namirh14x@gmail.com>
2023-08-24 00:26:50 -0400

    dgf

--- bot.py
+++ bot.py
@@ -252,16 +252,13 @@ async def sort_collection(msg, user):
         server_data[server_id]["user_collections"][user.id] = []
         
     def get_embed_value(embed):
-        for field in embed[3]:
-            if "Value:" in field[1]:
-                print(int(field[0].split()[1]))
-                return int(field[0].split()[1])
+        for player in collection:
+            for field in player[3]:
+                if "Value:" in field[1]:
+                    print(int(field[0].split()[1]))
+                    return int(field[0].split()[1])
                                   
     collection = server_data[server_id]["user_collections"][user.id]
-    for field in collection[3]:
-        if "Value:" in field[1]:
-            print(int(field[0].split()[1]))
-
     collection.sort(key=get_embed_value, reverse=True)
     
     await msg.channel.send("Your collection has been successfully sorted from highest to lowest value.")

943369c223d7c44fbd2c637a7d4067561e9f0e92
narikko <116518028+narikko@users.noreply.github.com>
2023-12-29 00:09:32 -0500

    :DSfg

--- src/bot.py
+++ src/bot.py
@@ -1226,7 +1226,7 @@ async def match_start(user, msg, other_id):
     server_id = str(msg.guild.id)
     user_id = str(user.id)
     
-    await msg.channel.send("Match Rules \u2611\ufe0f :" + "\n\n" + " • Winner takes all the money wagered. If the game ends in a draw, both players will receive back the money they wagered." + "\n\n" + " • Both teams will have shots on goal randomly throughout the match. A stronger defense and goalkeeper will increase the chances of a shot getting saved, while stronger forwards will contributing to higher goal scoring chance. Midfielders contribute both to defense and attack." + "\n\n" + " • Players participating in the match will be notified whenever one a player scores a goal or saves a shot.")
+    await msg.channel.send("Match Rules \u2611\ufe0f :" + "\n\n" + " • Winner takes all the money wagered. If the game ends in a draw, both players will receive back the money they wagered." + "\n\n" + " • Both teams will have shots on goal randomly throughout the match. A stronger defense and goalkeeper will increase the chances of a shot getting saved, while stronger forwards will contributing to higher goal scoring chance. Midfielders contribute both to defense and attack." + "\n\n" + " • Players participating in the match will be notified whenever a player scores a goal or saves a shot.")
     user_team_players = server_data[server_id]["user_team_players"][user_id] 
     other_team_players = server_data[server_id]["user_team_players"][other_id]
     
@@ -1278,8 +1278,9 @@ async def match_start(user, msg, other_id):
     other_d = int(sum(other_d_count) / len(other_d_count))
     
     await asyncio.sleep(1)
-    await msg.channel.send(f"**{user.name}'s Team Overall**:" + "\n" + f"__Attack__ \u2694\ufe0f : **{user_f}**" + "\n" + f"Midfield \u2694\ufe0f \U0001f6e1\ufe0f : **{user_m}**" + "\n" + f"Defence \U0001f6e1\ufe0f : {user_d}")
-    await msg.channel.send(f"**{client.get_user(int(other_id)).name}'s Team Overall**:" + "\n" + f"__Attack__ \u2694\ufe0f : **{other_f}**" + "\n" + f"Midfield \u2694\ufe0f \U0001f6e1\ufe0f : **{other_m}**" + "\n" + f"Defence \U0001f6e1\ufe0f : {other_d}")                
+    await msg.channel.send(f"**{user.name}'s Team Overall**:" + "\n" + f"__Attack__ \u2694\ufe0f : **{user_f}**" + "\n" + f"__Midfield__ \u2694\ufe0f \U0001f6e1\ufe0f : **{user_m}**" + "\n" + f"__Defence__ \U0001f6e1\ufe0f : **{user_d}**")
+    await asyncio.sleep(1)
+    await msg.channel.send(f"**{client.get_user(int(other_id)).name}'s Team Overall**:" + "\n" + f"__Attack__ \u2694\ufe0f : **{other_f}**" + "\n" + f"__Midfield__ \u2694\ufe0f \U0001f6e1\ufe0f : **{other_m}**" + "\n" + f"__Defence__ \U0001f6e1\ufe0f : **{other_d}**")                
                     
  
 async def trade_player(user, msg, player, mention):
